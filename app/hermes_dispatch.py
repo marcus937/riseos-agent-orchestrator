187fdc919d5f54d04e4e16a1a51e9b459051fde7
marcus937 <marcus@risecommercialdistrict.com>
2026-06-23 22:28:38 -0400

    Install Hermes redaction supplement

--- app/hermes_dispatch.py
+++ app/hermes_dispatch.py
@@ -3,9 +3,11 @@ from __future__ import annotations
 from app import hermes_dispatch_impl as _impl
 from app.hermes_evidence_patch import install_hermes_evidence_patch as _install_hermes_evidence_patch
 from app.hermes_preview_guard import install_preview_guard as _install_preview_guard
+from app.hermes_redaction_patch import install_hermes_redaction_patch as _install_hermes_redaction_patch
 
 _install_preview_guard(_impl, globals())
 _install_hermes_evidence_patch(_impl)
+_install_hermes_redaction_patch(_impl)
 
 for _name in dir(_impl):
     if not _name.startswith("__") and _name not in globals():
